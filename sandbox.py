74dc6f7a20b371663e43b9219180208f4cbbed0e
shashanksgr8 <shashanksgr8@gmail.com>
2021-06-02 23:47:44 +0530

    First trial with atom

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,22 @@
-# Sandbox area to test out code snippets before inclusion in cryptogram_solver.py.
-# Leave in code under testing, comment out code if needed for later
-# reference, or delete code that is no longer wanted.
+#_______________________________________________________________________________________________________________#
+#                                                                                                               #
+# Sandbox area to test out code snippets before inclusion in cryptogram_solver.py.                              #
+# Leave in code under testing, comment out code if needed for later                                             #
+# reference, or delete code that is no longer wanted.                                                           #
+# _______________________________________________________________________________________________________________#
+#                                      1 June 2021                                                              #
+# _______________________________________________________________________________________________________________#
+#                                                                                                               #
+#   10. ONCE ALL ARRAY ELEMENTS HAVE BEEN MATCHED COMPARE ELEMENTS TO THE DICTIONARY AND RE-ORDER ARRAY         #
+#                                                                                                               #
+#   11. CONCATENATE ARRAY INTO A PROPER SENTENCE                                                                #
+#_______________________________________________________________________________________________________________#
+
+import re
+
+name : str
+name = input('\nWhat shall I call you? \n\n')
+if re.search(r'^[a-zA-Z\s\D\.]+$', name):
+    print('\nHello' + ' ' + name + '!\n')
+else:
+    print('\nPlease avoid using any special charecters. \nYour name is special enough.\n\n')
